f5ff7952f8c87706fbe3ab97c01c37a6fda81198
Raul Bernardeli <raul@raulbcs.com.br>
2017-02-17 21:09:17 -0200

    LP3LAB 17/fev

--- 2017_1/LP3LAB/src/WebApplication1/src/java/mack/controllers/impl/BuscaUsuarioController.java
+++ 2017_1/LP3LAB/src/WebApplication1/src/java/mack/controllers/impl/BuscaUsuarioController.java
@@ -11,21 +11,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import mack.controllers.AbstractController;
 import mack.entities.Usuario;
+import mack.dao.usuario.UsuarioDAO;
+import mack.dao.usuario.UsuarioDAOFactory;
 
 /**
  *
  * @author rb
  */
-public class IndexController extends AbstractController {
+public class BuscaUsuarioController extends AbstractController {
     public void execute() {
         try {
+
+            UsuarioDAO dao = UsuarioDAOFactory.getUsuarioDAO();
             List usuarios = new ArrayList<Usuario>();
-            usuarios.add(new Usuario(1,"Mack","Junior"));
-            usuarios.add(new Usuario(2, "Mack","Neto"));
+
             // this.setReturnPage("/index.jsp"); // método não existente?
             this.getRequest().setAttribute("usuarios", usuarios);
         } catch(Exception ex) {
-            Logger.getLogger(IndexController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BuscaUsuarioController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
